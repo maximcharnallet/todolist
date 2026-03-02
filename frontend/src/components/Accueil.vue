849e10c072b91bca4af17f3e50b7e7832226f91f
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-03-02 14:00:30 +0100

    Add tasks

--- frontend/src/components/Accueil.vue
+++ frontend/src/components/Accueil.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
 
+  const newTask = ref ('')
+
+  const tasks = ref(['Tâche 1', 'Tâche 2'])
+
+  function addTask () {
+    if (newTask.value.trim()) {
+      tasks.value.push(newTask.value)
+      newTask.value = ''
+    }
+  }
+
+  console.log('tasks :', tasks)
 </script>
 <template>
   <v-layout class="rounded rounded-md border">
@@ -7,11 +20,16 @@
 
     <v-main class="d-flex align-center justify-center" height="300">
       <v-container>
-				<v-text-field label="Nouvelle tâche..." />
-				<v-list>
-					<v-list-item title="Tâche 1" />
-					<v-list-item title="Tâche 2" />
-      	</v-list>
+        <v-text-field v-model="newTask" clearable label="Ajouter..." @keydown.enter="addTask">
+          <template #append-inner>
+            <v-btn color="primary" variant="text" @click="addTask">
+              Ajouter
+            </v-btn>
+          </template>
+        </v-text-field>
+        <v-list>
+          <v-list-item v-for="task in tasks" :key="task" :title="task" />
+        </v-list>
       </v-container>
     </v-main>
   </v-layout>
