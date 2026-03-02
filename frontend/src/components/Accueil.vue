d5b5f00f9ae4e1113c137ee0b6f17f731959eef7
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-03-02 13:28:33 +0100

    Add .gitignore

--- frontend/src/components/Accueil.vue
+++ frontend/src/components/Accueil.vue
@@ -5,21 +5,13 @@
   <v-layout class="rounded rounded-md border">
     <v-app-bar title="Mes tâches à faire"></v-app-bar>
 
-    <v-navigation-drawer>
-      <v-list nav>
-        <v-list-item title="Ajouter une tâche" link></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-main class="d-flex align-center justify-center" height="300">
       <v-container>
-        <v-sheet
-          border="dashed md"
-          color="surface-light"
-          height="200"
-          rounded="lg"
-          width="100%"
-        ></v-sheet>
+				<v-text-field label="Nouvelle tâche..." />
+				<v-list>
+					<v-list-item title="Tâche 1" />
+					<v-list-item title="Tâche 2" />
+      	</v-list>
       </v-container>
     </v-main>
   </v-layout>
