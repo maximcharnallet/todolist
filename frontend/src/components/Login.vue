7423f8cf7e1ed979cc66a8e0985141091939dfea
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-03-03 13:43:42 +0100

    Add frontend Login

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,76 +1,92 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
 
+  const router = useRouter()
+  const name = ref('')
+  const password = ref('')
   const visible = ref(false)
+  const isError = ref(false)
+
+  async function handleSignin () {
+    const res = await fetch('/check_login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: name.value,
+        password: password.value,
+      }),
+    })
+    console.log(res)
+    if (res.status === 403) {
+      isError.value = true
+      setTimeout(() => {
+        isError.value = false
+      }, 3000)
+    } else if (res.status === 200) {
+      router.push('/accueil')
+    }
+  }
 </script>
 
 <template>
   <div>
     <v-card
-      class="mx-auto pa-12 pb-8"
+      class="mx-auto mt-12 pa-12 pb-8"
       elevation="3"
       max-width="448"
       rounded="lg"
     >
-      <div class="text-body-large text-medium-emphasis">Account</div>
+      <div class="text-body-large font-weight-bold text-medium-emphasis text-center">Se connecter</div>
+
+      <div class="text-body-large text-medium-emphasis">Nom</div>
 
       <v-text-field
+        v-model="name"
         density="compact"
-        placeholder="Email address"
-        prepend-inner-icon="mdi-email-outline"
+        required
         variant="outlined"
-      ></v-text-field>
+      />
 
       <div class="text-body-large text-medium-emphasis d-flex align-center justify-space-between">
-        Password
-
-        <a
-          class="text-body-small text-decoration-none text-blue"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Forgot login password?</a>
+        Mot de passe
       </div>
 
       <v-text-field
         :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
         density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
+        placeholder="Taper votre mot de passe"
+        required
+        :type="visible ? 'text' : 'password'"
         variant="outlined"
         @click:append-inner="visible = !visible"
-      ></v-text-field>
-
-      <v-card
-        class="mb-12"
-        color="surface-variant"
-        variant="tonal"
-      >
-        <v-card-text class="text-medium-emphasis text-body-small">
-          Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
-        </v-card-text>
-      </v-card>
+      />
 
       <v-btn
+        block
         class="mb-8"
         color="blue"
         size="large"
         variant="tonal"
-        block
+        @click="handleSignin()"
       >
-        Log In
+        Connexion
       </v-btn>
+
+      <v-alert v-if="isError" class="mb-4" type="error" variant="tonal">
+        Identifiants incorrects
+      </v-alert>
 
       <v-card-text class="text-center">
         <a
           class="text-blue text-decoration-none"
-          href="#"
+          href="/register"
           rel="noopener noreferrer"
           target="_blank"
         >
-          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          S'enregister <v-icon icon="mdi-chevron-right" />
         </a>
       </v-card-text>
     </v-card>
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,5 +1,59 @@
 <script setup lang="ts">
+  import { ref } from 'vue'
+
+  const visible = ref(false)
 </script>
+
 <template>
-  Register
+  <div>
+    <v-card
+      class="mx-auto mt-12 pa-12 pb-8"
+      elevation="3"
+      max-width="448"
+      rounded="lg"
+    >
+      <div class="text-body-large font-weight-bold text-medium-emphasis text-center">S'inscrire</div>
+
+      <div class="text-body-large text-medium-emphasis">Nom</div>
+
+      <v-text-field
+        density="compact"
+        variant="outlined"
+      />
+
+      <div class="text-body-large text-medium-emphasis d-flex align-center justify-space-between">
+        Mot de passe
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        density="compact"
+        placeholder="Taper votre mot de passe"
+        :type="visible ? 'text' : 'password'"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      />
+
+      <v-btn
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+      >
+        Connexion
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="/register"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          S'enregister <v-icon icon="mdi-chevron-right" />
+        </a>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
