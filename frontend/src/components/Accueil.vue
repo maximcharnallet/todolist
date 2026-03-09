31cfe1eae70e81d4b4075ef922d4246d78ef3267
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-03-09 14:51:20 +0100

    Revert "Add store for tasks"
    This reverts commit add5ad37fb5de56b6a5278ff2aa382eaf530dd5e.

--- frontend/src/components/Accueil.vue
+++ frontend/src/components/Accueil.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
   import { jwtDecode } from 'jwt-decode'
-  import { storeToRefs } from 'pinia'
-  import { ref } from 'vue'
-  // import { useTask } from '@/composables/useTask'
-  import { taskStore } from '@/stores/task.store'
+  import { onMounted, ref } from 'vue'
+  import { useTask } from '@/composables/useTask'
 
-  // const { tasks, isError, newTask, doAddTask, doGetTask, doDeleteTask } = useTask ()
+  const { tasks, isError, newTask, doAddTask, doGetTask, doDeleteTask } = useTask ()
 
   const token = localStorage.getItem('user_token')
 
@@ -16,40 +14,46 @@
     userName.value = decoded.name
   }
 
-  const title = ref('')
-  const store = taskStore()
-  const { tasks } = storeToRefs(store)
-
-  function addTask () {
-    store.addTask(title.value)
-    title.value = ''
+  async function fetchTasks () {
+    await doGetTask()
   }
+
+  onMounted(fetchTasks)
+
+  async function handleAddTask () {
+    await doAddTask(newTask.value)
+    newTask.value = ''
+    await doGetTask()
+  }
+
+  async function handleDeleteTask (id: string) {
+    await doDeleteTask(id)
+    await doGetTask()
+  };
 
 </script>
 <template>
   <v-layout class="rounded rounded-md border">
-    <v-app-bar :title="`Bonjour ${userName}`" />
+    <v-app-bar :title="`Bonjour ${userName}, voici tes tâches à faire`" />
+
     <v-main class="d-flex align-center justify-center" height="700">
       <v-container>
-        <v-text-field v-model="title" clearable label="Ajouter..." @keydown.enter="addTask">
+        <v-text-field v-model="newTask" clearable label="Ajouter..." @keydown.enter="handleAddTask()">
           <template #append-inner>
-            <v-btn color="primary" variant="text" @click="addTask">
+            <v-btn color="primary" variant="text" @click="handleAddTask()">
               Ajouter
             </v-btn>
           </template>
         </v-text-field>
-
         <v-list>
-          <v-list-item v-for="(task, index) in tasks" :key="index">
-            <template #prepend>
-              <v-checkbox-btn
-                v-model="task.done"
-                color="success"
-              />
+          <v-list-item v-for="task in tasks" :key="task._id" :title="task.title">
+            <template #append>
+              <v-btn
+                color="error"
+                variant="text"
+                @click="handleDeleteTask(task._id!)"
+              >Supprimer</v-btn>
             </template>
-            <v-list-item-title :class="{ 'text-decoration-line-through text-grey': task.done }">
-              {{ task.title }}
-            </v-list-item-title>
           </v-list-item>
         </v-list>
       </v-container>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,9 +4,6 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
-// Pinia
-import { createPinia } from 'pinia'
-
 // Composables
 import { createApp } from 'vue'
 
@@ -19,11 +16,8 @@ import App from './App.vue'
 // Styles
 import 'unfonts.css'
 
-const pinia = createPinia()
-
 const app = createApp(App)
 
 registerPlugins(app)
 
-app.use(pinia)
 app.mount('#app')
