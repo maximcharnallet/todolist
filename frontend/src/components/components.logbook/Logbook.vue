85a456ba9feee3cd8bd12966a23a17e040726847
Maxime Charnallet <maxime.charnallet@gmail.com>
2026-03-18 16:35:40 +0100

    Add dialog New Entry Logbook

--- frontend/src/components/components.logbook/Logbook.vue
+++ frontend/src/components/components.logbook/Logbook.vue
@@ -3,6 +3,7 @@
   import { storeToRefs } from 'pinia'
   import { onMounted, ref } from 'vue'
   import { logbookStore } from '@/store/logbook.store'
+  import NewEntry from './NewEntry.vue'
 
   const userName = ref('')
   const token = localStorage.getItem('user_token')
@@ -13,81 +14,58 @@
 
   const store = logbookStore()
 
-  const { newLog, logbook } = storeToRefs(store)
+  const { logbook } = storeToRefs(store)
 
-  async function handleAddLog () {
-    const dateTime = new Date(`${newLog.value.date}T${newLog.value.time}`)
-    await store.doAddLog({
-      description: newLog.value.description,
-      date: dateTime.toISOString(),
-      // severity: newLog.value.severity,
-      // type: newLog.value.type,
-    })
-  }
+  const dialog = ref(false)
 
   onMounted(() => {
     store.doGetLog()
     console.log('onMounted :', onMounted)
   })
+  async function openNewEntryDialog () {
+    dialog.value = true
+    return
+  }
 
 </script>
 
 <template>
   <v-container fluid>
     <h1 class="text-center pb-8">Main courante</h1>
-    <v-row justify="center">
-      <v-col cols="12" md="4">
-        <v-card class="pa-4" elevation="2">
-          <v-card-title>Nouvelle entrée</v-card-title>
-          <v-form>
-            <v-textarea v-model="newLog.description" label="Description" rows="3" />
-            <div class="d-flex ga-2">
-              <v-text-field v-model="newLog.date" label="Date" type="date" />
-              <v-text-field v-model="newLog.time" label="Heure" type="time" />
-            </div>
-            <!-- <v-select
-              v-model="newLog.type"
-              :items="['Accident', 'Soin', 'Observation', 'Logistique', 'Urgence']"
-              label="Type d'incident"
-            /> -->
-            <!-- <v-select
-              v-model="newLog.severity"
-              :items="['Bas', 'Moyen', 'Haut', 'Critique']"
-              label="Niveau de gravité"
-            /> -->
-            <v-btn block class="mt-4" color="primary" @click="handleAddLog">Envoyer</v-btn>
-          </v-form>
-        </v-card>
-      </v-col>
+    <v-btn
+      text="Modifier"
+      variant="tonal"
+      @click="openNewEntryDialog()"
+    > Nouvelle entrée
+    </v-btn>
+    <v-card class="pa-4" elevation="2">
+      <h3 class="mb-4 ml-4">Historique récent</h3>
+      <v-timeline align="start" side="end">
+        <v-timeline-item
+          v-for="log in logbook"
+          :key="log.date"
+          :dot-color="'info'"
+          size="small"
+        >
+          <template #opposite>
+            <span class="text-caption font-weight-bold">
+              {{ new Date(log.date).toLocaleString('fr-FR') }}
+            </span>
+          </template>
+          <v-card class="elevation-1">
+            <v-card-title class="text-subtitle-1">
+              <small>{{ log.userId }}</small>
+            </v-card-title>
 
-      <v-col cols="12" md="6">
-        <v-card class="pa-4" elevation="2">
-          <h3 class="mb-4 ml-4">Historique récent</h3>
-          <v-timeline align="start" side="end">
-            <v-timeline-item
-              v-for="log in logbook"
-              :key="log.date"
-              :dot-color="'info'"
-              size="small"
-            >
-              <template #opposite>
-                <span class="text-caption font-weight-bold">
-                  {{ new Date(log.date).toLocaleString('fr-FR') }}
-                </span>
-              </template>
-              <v-card class="elevation-1">
-                <v-card-title class="text-subtitle-1">
-                  <small>{{ log.userId }}</small>
-                </v-card-title>
-
-                <v-card-text class="text-body-2">
-                  {{ log.description }}
-                </v-card-text>
-              </v-card>
-            </v-timeline-item>
-          </v-timeline>
-        </v-card>
-      </v-col>
-    </v-row>
+            <v-card-text class="text-body-2">
+              {{ log.description }}
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </v-card>
+    <new-entry
+      v-model="dialog"
+    />
   </v-container>
 </template>
--- frontend/src/components/components.logbook/NewEntry.vue
+++ frontend/src/components/components.logbook/NewEntry.vue
@@ -1,5 +1,46 @@
 <script setup lang="ts">
+  import { storeToRefs } from 'pinia'
+  import { logbookStore } from '@/store/logbook.store'
+
+  const store = logbookStore()
+  const { newLog } = storeToRefs(store)
+
+  const isActive = defineModel<boolean>()
+
+  async function handleAddLog () {
+    const dateTime = new Date(`${newLog.value.date}T${newLog.value.time}`)
+    await store.doAddLog({
+      description: newLog.value.description,
+      date: dateTime.toISOString(),
+      // severity: newLog.value.severity,
+      // type: newLog.value.type,
+    })
+    isActive.value = false
+  }
+
 </script>
 <template>
-  Nouvelle entrée
+  <v-dialog v-model="isActive" max-width="600">
+    <v-card class="pa-4" elevation="2">
+      <v-card-title>Nouvelle entrée</v-card-title>
+      <v-form>
+        <v-textarea v-model="newLog.description" label="Description" rows="3" />
+        <div class="d-flex ga-2">
+          <v-text-field v-model="newLog.date" label="Date" type="date" />
+          <v-text-field v-model="newLog.time" label="Heure" type="time" />
+        </div>
+        <!-- <v-select
+          v-model="newLog.type"
+          :items="['Accident', 'Soin', 'Observation', 'Logistique', 'Urgence']"
+          label="Type d'incident"
+        /> -->
+        <!-- <v-select
+          v-model="newLog.severity"
+          :items="['Bas', 'Moyen', 'Haut', 'Critique']"
+          label="Niveau de gravité"
+        /> -->
+        <v-btn block class="mt-4" color="primary" @click="handleAddLog">Envoyer</v-btn>
+      </v-form>
+    </v-card>
+  </v-dialog>
 </template>
